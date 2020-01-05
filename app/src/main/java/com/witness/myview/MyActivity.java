dbaac5e3d021fea03cf2428f54a78cfb1463bbc8
witness <witness@ccwang-Mac.local>
2020-01-05 13:32:40 +0800

    测试王飞大佬第一次提交

---
+++ app/src/main/java/com/witness/myview/MyActivity.java
@@ -0,0 +1,18 @@
+package com.witness.myview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * @author: witness
+ * created: 2020-01-05
+ * desc:
+ */
+public class MyActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
