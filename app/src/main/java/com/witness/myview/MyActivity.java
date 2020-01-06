75173863db0368297651d9d6a95113e8228ef003
impp <516485107@qq.com>
2020-01-06 13:09:39 +0800

    1月6号测试提交

--- app/src/main/java/com/witness/myview/MyActivity.java
+++ app/src/main/java/com/witness/myview/MyActivity.java
@@ -13,6 +13,6 @@ public class MyActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_explosion);
     }
 }
