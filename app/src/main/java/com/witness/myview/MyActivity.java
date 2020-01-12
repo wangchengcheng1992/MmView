c72dfc101878078437caa09ca2333364bc14e3f9
impp <516485107@qq.com>
2020-01-12 11:49:26 +0800

    1月12号 测试主分支冲突提交

--- app/src/main/java/com/witness/myview/MyActivity.java
+++ app/src/main/java/com/witness/myview/MyActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ImageView;
 
 /**
  * @author: witness
@@ -19,5 +20,6 @@ public class MyActivity extends Activity {
         for (int i = 0; i < 10; i++) {
             Log.d("1111111111","11111");
         }
+        ImageView iv = findViewById(R.id.iv);
     }
 }
